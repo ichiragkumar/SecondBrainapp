c474cdff5c0e3cf2dfe50d3a065ba1e213e81151
ichiragkumar <chiragroy3030@gmail.com>
2025-01-07 15:21:30 +0530

    removed env

--- dist/models/tagModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose = require('mongoose');
-var tagSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique: true }
-});
-var Tag = mongoose.model('Tag', tagSchema);
-exports.default = Tag;
--- dist/zod/signup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.signupSchema = void 0;
-var zod_1 = require("zod");
-exports.signupSchema = zod_1.z.object({
-    name: zod_1.z.string().min(3).max(30).optional(),
-    email: zod_1.z.string().email(),
-    password: zod_1.z.string().min(8).max(30),
-});
